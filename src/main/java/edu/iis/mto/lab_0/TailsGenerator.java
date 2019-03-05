3ece0fc011dea75bcf1889e22c86667ca57a98ec
Hubert Liberadzki <Liberadzkih@gmail.com>
2019-03-05 18:45:55 +0100

    lab0 209377

--- src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
+++ src/main/java/edu/iis/mto/lab_0/TailsGenerator.java
@@ -6,7 +6,14 @@ import java.util.List;
 public class TailsGenerator {
 
     public List<String> tails(String value) {
-        return new ArrayList<>(value.length());
+        ArrayList word = new ArrayList<>();
+        if(value == null)
+            return word;
+        else {
+            for(int i=value.length(); i>=0; i--)
+                word.add(value.substring(0,i));
+            return word;
+        }
     }
 
 }
